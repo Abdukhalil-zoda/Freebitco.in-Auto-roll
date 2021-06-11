0cea7642fbf3ca9140a23b6c80a08f2a6561cf59
Abdulaziz <40052862+AbdulazizCyber@users.noreply.github.com>
2021-06-11 15:39:39 +0500

    REady to publish

---
+++ Chromium/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+using CefSharp.Wpf;
+using System.IO;
+
+namespace Chromium
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Бот для управление
+        /// </summary>
+        static TGBot bot = new TGBot("API");
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        /// <summary>
+        /// Основной браузер
+        /// </summary>
+        ChromiumWebBrowser chrome;
+        /// <summary>
+        /// Для скрытие
+        /// </summary>
+        System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            bot.Start();
+            CefSettings settings = new CefSettings();
+            settings.LogSeverity = LogSeverity.Verbose;
+            settings.CachePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache");
+            Cef.Initialize(settings);
+            chrome = new ChromiumWebBrowser("https://freebitco.in/?r=37959627");
+            grid.Children.Add(chrome);
+            ni.Icon = new System.Drawing.Icon("./favicon.ico");
+            ni.Visible = true;
+
+            bot.OnStart += Bot_OnStart;
+            chrome.FrameLoadEnd += Chrome_FrameLoadEnd;
+            ni.DoubleClick += delegate (object sender1, EventArgs args)
+            {
+                Show();
+                WindowState = WindowState.Normal;
+            };
+        }
+        /// <summary>
+        /// Обновляем страницу призапуске Auto-roll
+        /// </summary>
+        private void Bot_OnStart()
+        {
+            Refresh();
+        }
+        /// <summary>
+        /// Пороверка прикаждом обнавление
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Chrome_FrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+        {
+            var mess = "";
+            if (File.Exists("MESS"))
+            {
+                mess = File.ReadAllText("MESS");
+            }
+            bool check = mess != "0";
+            string script = string.Format("document.getElementById('free_play_form_button').style.display;");
+            
+            await chrome.EvaluateScriptAsync(script).ContinueWith(x =>
+            {
+                var response = x.Result;
+
+                if (response.Success && response.Result != null && check)
+                {
+                    var id = "play_without_captchas_button";
+                    var s = string.Format($"document.getElementById('{id}').style.display;");
+                    chrome.EvaluateScriptAsync(script).ContinueWith(xx =>
+                    {
+                        var res = xx.Result;
+                        if (res.Success && res.Result != null)
+                        {
+                            if (Convert.ToString(res.Result) != "none")
+                            {
+                                chrome.EvaluateScriptAsync($"document.getElementById('{id}').click();").Wait();
+                            }
+                        }
+                    });
+                    var resultres = Convert.ToString(response.Result);
+                    if (resultres != "none")
+                    {
+                        chrome.EvaluateScriptAsync("document.getElementById('free_play_form_button').click();").Wait();
+                    }
+                    else
+                    {
+                        Console.WriteLine("+++++++++++++++WAITforUPDATE++++++++++++++++++++++++");
+                        Console.WriteLine(DateTime.Now.ToString("HH:mm:ss.ff"));
+                    }
+                }
+            });
+        }
+
+        private void Refresh()
+        {
+            chrome.Reload();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var result = MessageBox.Show("Really close? Press no for background working", "Warning", MessageBoxButton.YesNo);
+            if (result != MessageBoxResult.Yes)
+            {
+                e.Cancel = true;
+                this.Hide();
+                ni.Visible = true;
+            }
+        }
+    }
+}
---
+++ Chromium/TGBot.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace Chromium
+{
+    public class TGBot
+    {
+        public TelegramBotClient bot;
+        public TGBot(string api)
+        { 
+            bot = new TelegramBotClient(api);
+            bot.OnMessage += Bot_OnMessage;
+        }
+
+        public void Start()
+        {
+            bot.StartReceiving();
+        }
+
+        public void Stop()
+        {
+            bot.StopReceiving();
+        }
+
+        public delegate void MethodContainer();
+        public event MethodContainer OnStop;
+        public event MethodContainer OnStart;
+        
+        private void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            
+            
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                File.WriteAllText("MESS", e.Message.Text);
+            }
+            if (e.Message.Text == "0")
+            {
+                OnStop();
+                bot.SendTextMessageAsync(e.Message.From.Id, "Bot stoped send any message to run Auto-roll", replyToMessageId: e.Message.MessageId);
+            }
+            else if (File.ReadAllText("MESS") == "0")
+            {
+                OnStart();
+                bot.SendTextMessageAsync(e.Message.From.Id, "Bot started send 0 to stop Auto-roll", replyToMessageId: e.Message.MessageId);
+            }
+            bot.SendTextMessageAsync(e.Message.From.Id, "Send 0 to stop Auto-roll");
+        }
+    }
+}
